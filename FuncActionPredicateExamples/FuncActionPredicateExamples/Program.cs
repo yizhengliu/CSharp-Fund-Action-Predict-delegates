5c6c1ffe526f5d01b744f4fd5376cd1b5fed3ef0
JiheiBot <2757174746@qq.com>
2023-12-26 16:32:46 +0000

    Explored predicate method
    predicate take a input and output a bool value, usually used to determine whether the value meets the criteria

--- FuncActionPredicateExamples/FuncActionPredicateExamples/Program.cs
+++ FuncActionPredicateExamples/FuncActionPredicateExamples/Program.cs
@@ -17,14 +17,69 @@ namespace FuncActionPredicateExamples
         {
             FuncPractice();
             ActionPractice();
-
+            PredicatePractice();
             Console.ReadKey();
         }
+
+        private static void PredicatePractice()
+        {
+            List<Employee> employees = new List<Employee>();
+            Action<int, string, string, decimal, char, bool> displayEmployeeRecords = (arg1, arg2, arg3, arg4, arg5, arg6) => Console.WriteLine($"Id: {arg1}{Environment.NewLine}First Name: {arg2}{Environment.NewLine}Last Name: {arg3}{Environment.NewLine}Annual salary: {arg4}{Environment.NewLine}Gender: {arg5}{Environment.NewLine}Manager: {arg6}");
+
+            employees.Add(new Employee { Id = 1, FirstName = "Sarah", LastName = "Jones", AnuualSalary = 60000, Gender = 'f', IsManager = true });
+            employees.Add(new Employee { Id = 2, FirstName = "Andrew", LastName = "Brown", AnuualSalary = 40000, Gender = 'm', IsManager = false });
+            employees.Add(new Employee { Id = 3, FirstName = "John", LastName = "Henderson", AnuualSalary = 58000, Gender = 'm', IsManager = true });
+            employees.Add(new Employee { Id = 4, FirstName = "Jane", LastName = "May", AnuualSalary = 30000, Gender = 'f', IsManager = false });
+
+            List<Employee> employeesFiletered = FilterEmployees(employees, e => e.IsManager);
+            foreach (Employee employee in employeesFiletered) 
+            {
+                displayEmployeeRecords(employee.Id, employee.FirstName, employee.LastName, employee.AnuualSalary, employee.Gender, employee.IsManager);
+                Console.WriteLine();
+            }
+        }   
+
+        static List<Employee> FilterEmployees(List<Employee> employees, Predicate<Employee> predicate) 
+        {
+            //predicate method encapsuluates a method that contains one parameter(intput) and returns bool,
+            //normally used to check whether the input meets the criteria 
+            List<Employee> employeesFiltered = new List<Employee>();
+
+            foreach (Employee employee in employees) 
+            {
+                if(predicate(employee))
+                { 
+                    employeesFiltered.Add(employee);
+                }
+            }
+            return employeesFiltered;
+        }
+
         private static void ActionPractice() 
         {
             //first trial, we can see that Action only take parameters and dont return any fields
-            Action<int, string, string> displayEmployeeRecords = (arg1, arg2, arg3) => Console.WriteLine($"Id: {arg1}{Environment.NewLine}First Name: {arg2}{Environment.NewLine}Last Name: {arg3}");
-            displayEmployeeRecords(1, "Sarah", "Jones");
+            //Action<int, string, string> displayEmployeeRecords = (arg1, arg2, arg3) => Console.WriteLine($"Id: {arg1}{Environment.NewLine}First Name: {arg2}{Environment.NewLine}Last Name: {arg3}");
+            //add annual salary
+            //Action<int, string, string, decimal> displayEmployeeRecords = (arg1, arg2, arg3, arg4) => Console.WriteLine($"Id: {arg1}{Environment.NewLine}First Name: {arg2}{Environment.NewLine}Last Name: {arg3}{Environment.NewLine}Annual salary: {arg4}");
+            //add Gender and is manager fields
+            //Action<int, string, string, decimal, char, bool> displayEmployeeRecords = (arg1, arg2, arg3, arg4, arg5, arg6) => Console.WriteLine($"Id: {arg1}{Environment.NewLine}First Name: {arg2}{Environment.NewLine}Last Name: {arg3}{Environment.NewLine}Annual salary: {arg4}{Environment.NewLine}Gender: {arg5}{Environment.NewLine}Manager: {arg6}");
+
+            //displayEmployeeRecords(1, "Sarah", "Jones", 60000, 'f', true);
+        }
+
+        public class Employee 
+        { 
+            public int Id { get; set; }
+
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+        
+            public decimal AnuualSalary { get; set; }
+            
+            public char Gender { get; set; }
+
+            public bool IsManager { get; set; }
         }
 
         private static void FuncPractice() 
@@ -69,6 +124,8 @@ namespace FuncActionPredicateExamples
             Console.WriteLine($"Total Salary: {calculateTotalAnnualSalary(60000, 2)}");
             */
         }
+
+        public static class Extensions
 
         public class MathClass 
         {
